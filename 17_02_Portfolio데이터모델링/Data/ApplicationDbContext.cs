23091af8ca3ac4d4a93b0bb9a2d36a01d46f4556
psd-inje <parksd74@hanmail.net>
2021-07-29 22:23:16 +0900

    ~17_02

---
+++ 17_02_Portfolio데이터모델링/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17_02_Portfolio데이터모델링.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ 17_02_Portfolio데이터모델링/Models/Portfolio.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace _17_02_Portfolio데이터모델링.Models
+{
+    public class Portfolio
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Portfolio>(this);
+        }
+    }
+}
