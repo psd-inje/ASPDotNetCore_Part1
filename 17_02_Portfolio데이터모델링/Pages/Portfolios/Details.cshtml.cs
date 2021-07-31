94f9cef7f0352c65afc8ba3b7113116d9158468b
psd-inje <parksd74@hanmail.net>
2021-07-31 11:43:00 +0900

    18_03

---
+++ 17_02_Portfolio데이터모델링/Pages/Portfolios/Details.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _17_02_Portfolio데이터모델링.Pages.Portfolios
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ 17_02_Portfolio데이터모델링/Pages/Portfolios/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _17_02_Portfolio데이터모델링.Pages.Portfolios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
