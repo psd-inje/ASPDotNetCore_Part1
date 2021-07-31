a98543f72157c15adf63bfb0f1a76a22c9a5cfc0
psd-inje <parksd74@hanmail.net>
2021-07-31 20:14:12 +0900

    19_06 코드정리

--- 17_02_Portfolio데이터모델링/Pages/Contact.cshtml.cs
+++ 17_02_Portfolio데이터모델링/Pages/Contact.cshtml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace _16_02_공통레이아웃적용하기.Pages
